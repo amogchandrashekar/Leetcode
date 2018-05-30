aba4abb139701ae06fa85c07ff7658fe3a71cedf
amogchandrashekar <amogmc3@gmail.com>
2018-05-30 11:38:25 +0530

    Add files via upload

---
+++ Easy/Longest Common Prefix.py
@@ -0,0 +1,15 @@
+import re
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ''
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            match = re.match(prefix, strs[i])
+            while not match:
+                prefix = prefix[:-1]
+                match = re.match(prefix, strs[i])
+        return prefix
+
+print(Solution().longestCommonPrefix(["flower","flow","flight"]))
