48d7d1e3734e277a8715a242c7dc171154e74941
Amog Chandrashekar <amogmc3@gmail.com>
2020-04-19 15:22:13 +0530

    Add dp code to solve unique path problem.

---
+++ Medium/Unique Paths II.py
@@ -0,0 +1,73 @@
+"""
+A robot is located at the top-left corner of a m x n grid.
+The robot can only move either down or right at any point in time.
+The robot is trying to reach the bottom-right corner of the grid.
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output:
+2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+"""
+
+from typing import List
+
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        # If the starting cell has an obstacle, then simply return as there would be
+        # no paths to the destination.
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        # Number of ways of reaching the starting cell = 1.
+        obstacleGrid[0][0] = 1
+
+        # Filling the values for the first column
+        for i in range(1,m):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+
+        # Filling the values for the first row
+        for j in range(1, n):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+
+        # Starting from cell(1,1) fill up the values
+        # No. of ways of reaching cell[i][j] = cell[i - 1][j] + cell[i][j - 1]
+        # i.e. From above and left.
+        for i in range(1,m):
+            for j in range(1,n):
+                if obstacleGrid[i][j] == 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                else:
+                    obstacleGrid[i][j] = 0
+
+        # Return value stored in rightmost bottommost cell. That is the destination.
+        return obstacleGrid[m-1][n-1]
+
+
+if __name__ == "__main__":
+    grid = [[1, 0]]
+    print(Solution().uniquePathsWithObstacles(grid))
