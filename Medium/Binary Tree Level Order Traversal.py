1048bc23474c2c8e3f268a695acf324727468f77
Amog Chandrashekar <amogmc3@gmail.com>
2020-05-01 19:20:10 +0530

    Solve using queues

---
+++ Medium/Binary Tree Level Order Traversal.py
@@ -0,0 +1,55 @@
+"""
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+"""
+
+
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        queue, answer = [root], list()
+        while queue:
+            answer.append([node.val for node in queue])
+            temp = list()
+            for node in queue:
+                temp.extend([node.left, node.right])
+            queue = [node for node in temp if node]
+        return answer
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.right.right = TreeNode(6)
+    root.right.right.left = TreeNode(9)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.left.right.left = TreeNode(7)
+    root.left.right.right = TreeNode(8)
+    print(Solution().levelOrder(root))
