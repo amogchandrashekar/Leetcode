9d5ae7de2cce21e8b43fc626c0f4100c3aec298e
Amog Chandrashekar <amogmc3@gmail.com>
2020-05-18 22:17:59 +0530

    LC Weekly 189 | Set intersection

---
+++ Medium/People Whose List of Favorite Companies Is Not a Subset of Another List.py
@@ -0,0 +1,55 @@
+"""
+Given the array favoriteCompanies where favoriteCompanies[i] is the list of favorites companies for the ith person (indexed from 0).
+Return the indices of people whose list of favorite companies is not a subset of any other list of favorites companies. You must return the indices in increasing order.
+
+
+
+Example 1:
+
+Input: favoriteCompanies = [["leetcode","google","facebook"],["google","microsoft"],["google","facebook"],["google"],["amazon"]]
+Output: [0,1,4]
+Explanation:
+Person with index=2 has favoriteCompanies[2]=["google","facebook"] which is a subset of favoriteCompanies[0]=["leetcode","google","facebook"] corresponding to the person with index 0.
+Person with index=3 has favoriteCompanies[3]=["google"] which is a subset of favoriteCompanies[0]=["leetcode","google","facebook"] and favoriteCompanies[1]=["google","microsoft"].
+Other lists of favorite companies are not a subset of another list, therefore, the answer is [0,1,4].
+Example 2:
+
+Input: favoriteCompanies = [["leetcode","google","facebook"],["leetcode","amazon"],["facebook","google"]]
+Output: [0,1]
+Explanation: In this case favoriteCompanies[2]=["facebook","google"] is a subset of favoriteCompanies[0]=["leetcode","google","facebook"], therefore, the answer is [0,1].
+Example 3:
+
+Input: favoriteCompanies = [["leetcode"],["google"],["facebook"],["amazon"]]
+Output: [0,1,2,3]
+
+
+Constraints:
+
+1 <= favoriteCompanies.length <= 100
+1 <= favoriteCompanies[i].length <= 500
+1 <= favoriteCompanies[i][j].length <= 20
+All strings in favoriteCompanies[i] are distinct.
+All lists of favorite companies are distinct, that is, If we sort alphabetically each list then favoriteCompanies[i] != favoriteCompanies[j].
+All strings consist of lowercase English letters only.
+"""
+from typing import List
+
+
+class Solution:
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
+        ans = []
+        for ind, comp_list in enumerate(favoriteCompanies):
+            is_unique = True
+            for comp_ind, comp in enumerate(favoriteCompanies):
+                if ind != comp_ind:
+                    if len(set(comp_list).intersection(comp)) == len(comp_list):
+                        is_unique = False
+                        break
+            if is_unique:
+                ans.append(ind)
+        return ans
+
+
+if __name__ == "__main__":
+    favoriteCompanies = [["leetcode", "google", "facebook"], ["leetcode", "amazon"], ["facebook", "google"]]
+    print(Solution().peopleIndexes(favoriteCompanies))
