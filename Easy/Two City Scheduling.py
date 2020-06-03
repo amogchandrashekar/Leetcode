fbf1a672384be28a08ce287f66448d7aceb438d6
Amog Chandrashekar <amogmc3@gmail.com>
2020-06-03 15:02:00 +0530

    June LC 30 challenge

---
+++ Easy/Two City Scheduling.py
@@ -0,0 +1,35 @@
+"""
+There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
+
+Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
+
+
+
+Example 1:
+
+Input: [[10,20],[30,200],[400,50],[30,20]]
+Output: 110
+Explanation:
+The first person goes to city A for a cost of 10.
+The second person goes to city A for a cost of 30.
+The third person goes to city B for a cost of 50.
+The fourth person goes to city B for a cost of 20.
+
+The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+
+
+Note:
+
+1 <= costs.length <= 100
+It is guaranteed that costs.length is even.
+1 <= costs[i][0], costs[i][1] <= 1000
+"""
+from typing import List
+
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda cost: cost[0] - cost[1])
+        costs_for_A = sum([cost[0] for cost in costs[:len(costs) // 2]])
+        costs_for_B = sum([cost[1] for cost in costs[len(costs) // 2:]])
+        return costs_for_A + costs_for_B
