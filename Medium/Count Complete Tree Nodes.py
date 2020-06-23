006cb78385429da65cd6e198ae0d6b646327d041
Amog Chandrashekar <amogmc3@gmail.com>
2020-06-23 13:13:58 +0530

    June lc 30 challenge

---
+++ Medium/Count Complete Tree Nodes.py
@@ -0,0 +1,68 @@
+"""
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled,
+and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input:
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        ## APPROACH : RECURSION ##
+        ## TIME COMPLEXICITY : LOG N * LOG N ##
+
+        ## LOGIC ##
+        # If left sub tree height equals right sub tree height then,
+        #       a. left sub tree is perfect binary tree
+        #       b. right sub tree is complete binary tree
+        # If left sub tree height greater than right sub tree height then,
+        #       a. left sub tree is complete binary tree
+        #       b. right sub tree is perfect binary tree
+
+        if not root:
+            return 0
+
+        def depthLeft(node):
+            d = 0
+            while node:
+                d += 1
+                node = node.left
+            return d
+
+        def depthRight(node):
+            d = 0
+            while node:
+                d += 1
+                node = node.right
+            return d
+
+        ld = depthLeft(root.left)
+        rd = depthRight(root.right)
+
+        if ld == rd:
+            return 2 ** (ld + 1) - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
