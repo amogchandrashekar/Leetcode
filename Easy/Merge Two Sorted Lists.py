9293b36f3a15801c1351cf02adc616a66bc0089f
amog <amog@streamingo.ai>
2021-05-06 16:59:26 +0530

    use recursion

---
+++ Easy/Merge Two Sorted Lists.py
@@ -0,0 +1,25 @@
+"""
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the
+nodes of the first two lists.
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 or not l2:
+            return l1 or l2
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
