b55596365ff000e16d0d45b4f481442f8914c91c
amog <amog@streamingo.ai>
2021-06-26 19:20:27 +0530

    solve using recursion

---
+++ Easy/Reverse Linked List.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        node = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return node
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    ans = Solution().reverseList(head)
+    while ans:
+        print(f'curr node : {ans.val}')
+        ans = ans.next
