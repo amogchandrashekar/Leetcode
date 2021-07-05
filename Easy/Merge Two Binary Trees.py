b81702d7880b64cd9f7b8da5e9b6116be9e2ad4e
amog <amog@streamingo.ai>
2021-07-05 09:20:50 +0530

    solve using recursion

---
+++ Easy/Merge Two Binary Trees.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if not root1 or not root2:
+            return root1 or root2
+
+        root = TreeNode(root1.val + root2.val)
+        root.left = self.mergeTrees(root1.left, root2.left)
+        root.right = self.mergeTrees(root1.right, root2.right)
+        return root
+
+
+if __name__ ==  '__main__':
+    root1 = TreeNode(1)
+    root1.left = TreeNode(3)
+    root1.right = TreeNode(2)
+    root1.left.left = TreeNode(5)
+
+    root2 = TreeNode(2)
+    root2.left = TreeNode(1)
+    root2.right = TreeNode(3)
+    root2.left.right = TreeNode(4)
+    root2.right.right = TreeNode(7)
+
+    ans = Solution().mergeTrees(root1, root2)
+    a = []
