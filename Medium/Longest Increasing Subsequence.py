5ea31cea6b1a101d2b8c243e989f2ecd022bf0e4
amog <amog@streamingo.ai>
2021-07-10 15:33:01 +0530

    add memoization code

--- Medium/Longest Increasing Subsequence.py
+++ Medium/Longest Increasing Subsequence.py
@@ -16,6 +16,7 @@ from typing import List
 
 
 class Solution:
+
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1 for _ in range(len(nums))]
         for i in range(len(dp)):
@@ -23,7 +24,27 @@ class Solution:
                 dp[i] = max(dp[j] + 1 if nums[j] < nums[i] else 0, dp[i])
         return max(dp) if dp else 0
 
+    def lengthOfLIS_memoization(self, nums: List[int]) -> int:
+        memo = dict()
+
+        def dfs(ind, prev_max):
+            if (ind, prev_max) in memo:
+                return memo[(ind, prev_max)]
+
+            if ind == len(nums):
+                return 0
+
+            cur_num = nums[ind]
+            if cur_num > prev_max:
+                memo[(ind, prev_max)] = max(1 + dfs(ind + 1, cur_num), dfs(ind + 1, prev_max))
+            else:
+                memo[(ind, prev_max)] = dfs(ind + 1, prev_max)
+
+            return memo[(ind, prev_max)]
+
+        return dfs(0, float('-inf'))
+
 
 if __name__ == "__main__":
-    nums = [1, 3, 6, 7, 9, 4, 10, 5, 6]
+    nums = [0,1,0,3,2,3]
     print(Solution().lengthOfLIS(nums))
