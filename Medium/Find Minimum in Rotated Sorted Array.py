d67373f7ac661a0f66e743c7b336d7eedab0fe91
amog <amog@streamingo.ai>
2021-07-11 14:56:33 +0530

    use binary search to find pivot on which array was rotated

---
+++ Medium/Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[l] <= nums[m] <= nums[r]:
+                return l
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+
+
+if __name__ == '__main__':
+    nums = [3, 1, 2]
+    print(Solution().findMin(nums))
