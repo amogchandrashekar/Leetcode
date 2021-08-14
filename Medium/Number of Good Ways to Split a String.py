42b357b58eb9ea3d8f289503c964e8a40d033009
amog <amog@streamingo.ai>
2021-08-14 10:25:54 +0530

    solve using sliding window

---
+++ Medium/Number of Good Ways to Split a String.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+class Solution:
+    def numSplits(self, s: str) -> int:
+
+        left_count = collections.Counter()
+        right_count = collections.Counter(s)
+        res = 0
+
+        for c in s:
+
+            left_count[c] += 1
+            right_count[c] -= 1
+
+            if right_count[c] == 0:
+                del right_count[c]
+
+            if len(left_count) == len(right_count):
+                res += 1
+
+        return res
