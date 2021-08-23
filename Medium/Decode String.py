2ec40606a4019777a541fe0b1d34fe1f96b2a676
amog <amogmc3@gmail.com>
2021-08-23 22:10:03 +0530

    solve using stack

--- Medium/Decode String.py
+++ Medium/Decode String.py
@@ -1,34 +1,29 @@
 class Solution:
     def stacky(self, s):
-        """
-        When we hit an open bracket, we know we have parsed k for the contents of the bracket, so
-        push (current_string, k) to the stack, so we can pop them on closing bracket to duplicate
-        the enclosed string k times.
-        """
         stack = []
-        current_string = ""
-        k = 0
 
         for char in s:
-            if char == "[":
-                # Just finished parsing this k, save current string and k for when we pop
-                stack.append((current_string, k))
-                # Reset current_string and k for this new frame
-                current_string = ""
-                k = 0
-            elif char == "]":
-                # We have completed this frame, get the last current_string and k from when the frame
-                # opened, which is the k we need to duplicate the current current_string by
-                last_string, last_k = stack.pop(-1)
-                current_string = last_string + last_k * current_string
-            elif char.isdigit():
-                k = k * 10 + int(char)
-            else:
-                current_string += char
 
-        return current_string
+            if char != ']':
+                stack.append(char)
+
+            else:
+                decoded_text = ''
+                number = ''
+
+                while stack and stack[-1].isalpha():
+                    decoded_text = stack.pop() + decoded_text
+
+                stack.pop()
+
+                while stack and stack[-1].isnumeric():
+                    number = stack.pop() + number
+
+                stack.append(decoded_text * int(number))
+
+        return "".join(stack)
 
 
 if __name__ == '__main__':
-    s = "3[a20[c]]"
+    s = "3[a2[c]]"
     print(Solution().stacky(s))
