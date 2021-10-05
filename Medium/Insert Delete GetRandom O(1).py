3dd8490f2e7b0c67b9575db60b29d4bc11ae462c
amog <amogmc3@gmail.com>
2021-10-05 21:57:24 +0530

    solve using hash map and random on array

--- Medium/Insert Delete GetRandom O(1).py
+++ Medium/Insert Delete GetRandom O(1).py
@@ -1,4 +1,4 @@
-from random import choice
+from random import randint
 
 
 class RandomizedSet:
@@ -7,34 +7,51 @@ class RandomizedSet:
         """
         Initialize your data structure here.
         """
-        self.set = set()
+        self.hash_map = dict()
+        self.arr = list()
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val not in self.set:
-            self.set.add(val)
-            return True
-        return False
+        if val in self.hash_map:
+            return False
+        self.hash_map[val] = len(self.arr)
+        self.arr.append(val)
+        return True
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.set:
-            self.set.remove(val)
-            return True
-        return False
+        if val not in self.hash_map:
+            return False
+        ind = self.hash_map.pop(val)
+        swap_ind = len(self.arr) - 1
+        self.arr[ind], self.arr[swap_ind] = self.arr[swap_ind], self.arr[ind]
+        self.arr.pop()
+        if ind < len(self.arr):
+            self.hash_map[self.arr[ind]] = ind
+        return True
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return choice(tuple(self.set))
+        random_ind = randint(0, len(self.arr) - 1)
+        return self.arr[random_ind]
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+if __name__ == '__main__':
+    rset = RandomizedSet()
+    print(rset.remove(0))
+    print(rset.remove(0))
+    print(rset.insert(0))
+    print(rset.getRandom())
+    print(rset.remove(0))
+    print(rset.insert(0))
